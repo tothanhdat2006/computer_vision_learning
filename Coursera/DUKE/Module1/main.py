fbeae42fbb98fcca5405ad75dbbbbd4f13bd153f
ToThanhDat <tothanhdat1421@gmail.com>
2025-02-13 11:45:40 +0700

    Done compress without encoding

--- Coursera/DUKE/Module1/main.py
+++ Coursera/DUKE/Module1/main.py
@@ -4,16 +4,24 @@ import argparse
 import cv2
 import matplotlib.pyplot as plt
 
-from utils.jpeg_compression import get_blocks, forward_transformation, quantization
+from utils.jpeg_compression import get_blocks, forward_transformation, quantization, inverse_quantization, inverse_transformation
 
 def jpeg_compress(img: np.ndarray) -> np.ndarray:
+    block_size = 8
     # Pipeline 
-    blocks = get_blocks(img)
+    blocks = get_blocks(img, block_size=block_size)
     blocks = [forward_transformation(block, type="DCT") for block in blocks]
     blocks = [quantization(block, type="normal") for block in blocks]
     
     # Reconstruction
-    return [] # in progress
+    inversed_block = [inverse_quantization(block, type="normal") for block in blocks]
+    inversed_block = [inverse_transformation(block, type="DCT") for block in blocks]
+
+    reconstructed_image = np.zeros_like(img)
+    for i in range(0, img.shape[0], block_size):
+        for j in range(0, img.shape[1], block_size):
+            reconstructed_image[i:i+block_size, j:j+block_size] = inversed_block.pop(0)
+    return reconstructed_image # in progress
 
 
 def get_args():
@@ -35,7 +43,9 @@ if __name__ == "__main__":
         if args.resize:
             original_image = cv2.resize(original_image, (args.resize[0], args.resize[1]))
             
-        compressed_image = jpeg_compress(original_image)
+        reconstruct_image = jpeg_compress(original_image)
+        mse = np.mean((original_image - reconstruct_image)**2)
+        print(f"Mean Squared Error: {mse}")
 
         plt.figure(figsize=(12, 6))
         
@@ -45,7 +55,7 @@ if __name__ == "__main__":
 
         plt.subplot(1, 2, 2)
         plt.title("Compressed Image")
-        plt.imshow(compressed_image, cmap='gray')
+        plt.imshow(reconstruct_image, cmap='gray')
         
         plt.tight_layout()
         plt.show()
--- Coursera/DUKE/Module1/utils/jpeg_compression.py
+++ Coursera/DUKE/Module1/utils/jpeg_compression.py
@@ -1,7 +1,16 @@
 import numpy as np
 
-# O(nm) time complexity
 def get_blocks(img: np.ndarray, block_size: int=8) -> np.ndarray:
+    '''
+    Get blocks of (block_size, block_size) from image
+
+    Args:
+    - img: image to be divided into blocks
+    - block_size: size of block
+
+    Returns:
+    - blocks: list of blocks
+    '''
     blocks = []
     if img.shape[0] % block_size != 0 or img.shape[1] % block_size != 0:
         pad_row = block_size - img.shape[0] % block_size
@@ -14,8 +23,18 @@ def get_blocks(img: np.ndarray, block_size: int=8) -> np.ndarray:
 
     return blocks
 
-# O((nm)^2) time complexity
 def forward_transformation(block: np.ndarray, type: str="DCT") -> np.ndarray:
+    '''
+    DCT or DFT transformation of block
+
+    Args:
+    - block: block to be transformed
+    - type: type of transformation
+
+    Returns:
+    - transformed block: block after transformation
+    '''
+    transformed_block = []
     if type == "DCT":
         block = block.astype(np.float64)
         dct_block = np.zeros_like(block, dtype=np.float64)
@@ -38,14 +57,26 @@ def forward_transformation(block: np.ndarray, type: str="DCT") -> np.ndarray:
                 
                 dct_block[u, v] = c_u * c_v * sum_block
         
-        return dct_block
+        transformed_block = dct_block
     
     elif type == "DFT":
         # DFT
-        return []
+        transformed_block = []
 
-# O(1) time complexity
+    return transformed_block
+    
 def quantization(block: np.ndarray, type: str="normal", quantization_table_custom: np.ndarray=None) -> np.ndarray:
+    '''
+    Quantization of block
+
+    Args: 
+    - block: block to be quantized
+    - type: type of quantization
+    - quantization_table_custom: custom quantization table
+
+    Returns:
+    - quantized block: block after quantization
+    '''
     quantization_table = []
     if type == "normal":
         quantization_table = np.array([[16, 11, 10, 16, 24, 40, 51, 61],
@@ -61,4 +92,78 @@ def quantization(block: np.ndarray, type: str="normal", quantization_table_custo
             raise ValueError("quantization_table_custom must be provided")
         quantization_table = quantization_table_custom
         
-    return np.round(np.divide(block, quantization_table))
+    quantized_block = np.round(block / quantization_table)
+    return quantized_block
+
+def inverse_transformation(block: np.ndarray, type: str="DCT") -> np.ndarray:
+    '''
+    Inverse transformation of DCT or DFT
+
+    Args:
+    - block: transformed block to be inverse transformed
+    - type: type of transformation
+
+    Returns:
+    - inverse transformed block: block after inverse transformation
+    '''
+    inverse_transformed_block = []
+    if type == "DCT":
+        block = block.astype(np.float64)
+        inverse_dct_block = np.zeros_like(block, dtype=np.float64)
+        
+        m = block.shape[0] # 8 for 8x8 block
+        n = block.shape[1] # 8 for 8x8 block
+        
+        for x in range(m):
+            for y in range(n):
+                sum_block = 0
+                for u in range(m):
+                    c_u = 1 / np.sqrt(m) if u == 0 else np.sqrt(2) / np.sqrt(m)
+                    for v in range(n):
+                        c_v = 1 / np.sqrt(n) if v == 0 else np.sqrt(2) / np.sqrt(n)
+                        cos1 = np.cos((2*x + 1)*u*np.pi / (2 * m))
+                        cos2 = np.cos((2*y + 1)*v*np.pi / (2 * n))
+                        sum_block += c_u * c_v * block[u, v] * cos1 * cos2
+                
+                inverse_dct_block[x, y] = sum_block
+        
+        inverse_dct_block = np.clip(inverse_dct_block, 0, 255) # Clip values to valid range
+        inverse_dct_block = inverse_dct_block.astype(np.uint8) # Convert back to uint8
+        inverse_transformed_block = inverse_dct_block
+    
+    elif type == "DFT":
+        # DFT
+        inverse_transformed_block = []
+
+    return inverse_transformed_block
+
+
+def inverse_quantization(quantized_block: np.ndarray, type: str="normal", quantization_table_custom: np.ndarray=None) -> np.ndarray:
+    '''
+    Inverse quantization of block
+
+    Args:
+    - quantized_block: quantized block to be inverse quantized
+    - type: type of quantization
+    - quantization_table_custom: custom quantization table
+
+    Returns:
+    - inverse quantized block: block after inverse quantization
+    '''
+    quantization_table = []
+    if type == "normal":
+        quantization_table = np.array([[16, 11, 10, 16, 24, 40, 51, 61],
+                                        [12, 12, 14, 19, 26, 58, 60, 55],
+                                        [14, 13, 16, 24, 40, 57, 69, 56],
+                                        [14, 17, 22, 29, 51, 87, 80, 62],
+                                        [18, 22, 37, 56, 68, 109, 103, 77],
+                                        [24, 35, 55, 64, 81, 104, 113, 92],
+                                        [49, 64, 78, 87, 103, 121, 120, 101],
+                                        [72, 92, 95, 98, 112, 100, 103, 99]])
+    elif type == "custom":
+        if quantization_table_custom is None:
+            raise ValueError("quantization_table_custom must be provided")
+        quantization_table = quantization_table_custom
+        
+    inverse_quantization_block = quantized_block * quantization_table
+    return inverse_quantization_block
